86a72f3a9a375c22dca1e5ee41be2e1e1dd90117
B1910668-TN <ngab1910668@student.ctu.edu.vn>
2022-10-25 08:15:23 +0700

    Upload commit

---
+++ administrator/cache/_media_version/19cc46dde21526bcc69ab06f6d83343b-cache-_media_version-f8b324007d41d1decfd00f7c4239cb3c.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"29c124fd6277dc9821f5db404ed81339";s:6:"output";s:0:"";}
---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-10-25 00:49:46 UTC
+#Software: Joomla! 4.2.2 Stable [ Uaminifu ] 2-September-2022 20:41 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-10-25T00:49:46+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2022-10-25T00:49:50+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
